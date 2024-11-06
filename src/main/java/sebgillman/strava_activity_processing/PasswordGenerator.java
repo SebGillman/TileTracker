127b3dd09eba78e97a1f3795eb2a830555f2e98a
SebGillman <sebinred1@gmail.com>
2024-11-06 18:20:07 +0000

    feat: add password generator class

---
+++ src/main/java/sebgillman/strava_activity_processing/PasswordGenerator.java
@@ -0,0 +1,19 @@
+package sebgillman.strava_activity_processing;
+
+import java.security.SecureRandom;
+
+public class PasswordGenerator {
+
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final SecureRandom RANDOM = new SecureRandom();
+
+    public static String generateRandomString(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int randomIndex = RANDOM.nextInt(CHARACTERS.length());
+            sb.append(CHARACTERS.charAt(randomIndex));
+        }
+        return sb.toString();
+
+    }
+}
